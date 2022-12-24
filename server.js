1f4a402b12b8adb90d3b422f78d5f2872323229b
Yash Pandey <yashpandey2727@gmail.com>
2022-12-24 12:22:35 +0530

    restarting server

--- server.js
+++ server.js
@@ -1,56 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectionDB = require('./server/database/connection');
-const routes = require('./server/routes/router');
-const mongoose = require('mongoose');
+const connectionDB = require("./server/database/connection");
+const routes = require("./server/routes/router");
+const mongoose = require("mongoose");
 
-//connecting mongoDB 
+//connecting mongoDB
 connectionDB();
 
-//parsing 
-app.use(express.urlencoded({extended: true}));
+//parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 //routing
 app.use(routes);
 
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose connection is disconnected...')
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection is disconnected...");
+});
 
-//printing the process id 
+//printing the process id
 const server = app.listen(process.env.PORT || 3010, () => {
-    console.log(process.pid);
-    console.log(`on process id: ${process.pid}`);
+  console.log(process.pid);
+  console.log(`on process id: ${process.pid}`);
 });
 
 //gracefully shutting down the server
 //it is executed when ctrl c is pressed
-process.on('SIGINT', ()=>{
-   console.log('SIGINT recieved.')
-   server.close(()=>{
-    console.log('server is closed...')
+process.on("SIGINT", () => {
+  console.log("SIGINT recieved.");
+  server.close(() => {
+    console.log("server is closed...");
     mongoose.connection.close(false, () => {
-        process.exit(0)
-    })
-   })
-})
+      process.exit(0);
+    });
+  });
+});
 
 //it executes when kill statement is executed
-process.on('SIGTERM', ()=>{
-    console.log('SIGTERM recieved.')
-    server.close(()=>{
-        console.log('server is closed...')
-        mongoose.connection.close(false, () => {
-            process.exit(0)
-        })
-       })
-})
-
-
+process.on("SIGTERM", () => {
+  console.log("SIGTERM recieved.");
+  server.close(() => {
+    console.log("server is closed...");
+    mongoose.connection.close(false, () => {
+      process.exit(0);
+    });
+  });
+});
